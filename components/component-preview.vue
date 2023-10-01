6c25d74f1e04845f3a67ad0a88224c739afa4f03
Evren Ceyhan <evren@ceyhan.io>
2023-10-01 13:47:58 +0200

    component preview bg flag added

--- components/component-preview.vue
+++ components/component-preview.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+interface Props {
+  withBackground?: boolean;
+}
+
+defineProps<Props>();
+</script>
+
 <template>
   <div class="text-base-content my-6 lg:my-12">
     <!-- title -->
@@ -13,9 +21,13 @@
 
     <!-- preview -->
     <div class="grid">
-      <div class="bg-base-300 rounded-b-box rounded-tr-box relative overflow-x-auto">
+      <div class="bg-base-300 border border-base-300 rounded-box relative overflow-x-auto">
         <div
-          class="preview border-base-300 bg-base-100 rounded-b-box rounded-tr-box flex min-h-[6rem] min-w-[18rem] max-w-4xl flex-wrap items-center justify-center gap-2 overflow-x-hidden border bg-cover bg-top p-4"
+          class="flex min-h-[6rem] min-w-[18rem] max-w-4xl flex-wrap items-center justify-center gap-2 overflow-x-hidden p-4"
+          :class="{
+            'preview-pattern': !withBackground,
+            'preview-background': withBackground,
+          }"
         >
           <slot />
         </div>
@@ -25,7 +37,7 @@
 </template>
 
 <style scoped>
-.preview {
+.preview-pattern {
   background-image: repeating-linear-gradient(
     45deg,
     hsl(var(--b1)),
@@ -33,5 +45,10 @@
     hsl(var(--b2)) 13px,
     hsl(var(--b2)) 14px
   );
+}
+
+.preview-background {
+  background-image: url("/img/preview-bg.jpg");
+  background-position: top center;
 }
 </style>
