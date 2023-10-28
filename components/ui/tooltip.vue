e6837eb042aaed53c0882a573a3bea19ae3d6a0f
Evren Ceyhan <evren.ceyhan@gmail.com>
2023-10-28 14:57:24 +0200

    tooltip directive snippet added (commented)

--- components/ui/tooltip.vue
+++ components/ui/tooltip.vue
@@ -18,6 +18,31 @@ const POSITION_CLASS_MAP: ClassMap<Position> = {
   left: "tooltip-left",
   right: "tooltip-right",
 };
+
+interface Bindings {
+  text?: string;
+  open?: boolean;
+  color?: Color;
+  position?: Position;
+}
+
+// not working because it's not auto registered
+// you should put it inside a plugin /  module
+//
+//
+// useNuxtApp().vueApp.directive("tooltip", (el, binding) => {
+//   // this will be called for both `mounted` and `updated`
+//   const { open, color, position, text } = binding.value as Bindings;
+
+//   // if no text, remove tooltip
+//   if (!text) return;
+
+//   el.setAttribute("data-tip", text);
+//   el.classList.add("tooltip");
+//   el.classList.add(COLOR_CLASS_MAP[color ?? "default"]);
+//   el.classList.add(POSITION_CLASS_MAP[position ?? "top"]);
+//   open && el.classList.add("tooltip-open");
+// });
 </script>
 
 <script setup lang="ts">
