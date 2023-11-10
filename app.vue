9cc90603e0f95832b817a376420a49f71c7a02db
Evren Ceyhan <evren.ceyhan@gmail.com>
2023-11-10 09:52:39 +0100

    Add Github repo nav link

--- app.vue
+++ app.vue
@@ -125,30 +125,32 @@ const menus = [
     </template>
 
     <div
-      class="bg-base-100 text-base-content sticky top-0 z-30 flex h-16 w-full justify-center bg-opacity-90 backdrop-blur transition-all duration-100 shadow-sm"
-    >
+      class="bg-base-100 text-base-content sticky top-0 z-30 flex h-16 w-full justify-center bg-opacity-90 backdrop-blur transition-all duration-100 shadow-sm">
       <nav class="navbar w-full">
-        <!-- drawer toggle -->
-        <label for="app-drawer" class="btn btn-square btn-ghost drawer-button lg:hidden">
-          <svg
-            width="20"
-            height="20"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            class="inline-block h-5 w-5 stroke-current md:h-6 md:w-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            ></path>
-          </svg>
-        </label>
+        <div class="flex flex-1 md:gap-1 lg:gap-2">
+          <!-- drawer toggle -->
+          <label for="app-drawer" class="btn btn-square btn-ghost drawer-button lg:hidden">
+            <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+              class="inline-block h-5 w-5 stroke-current md:h-6 md:w-6">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+            </svg>
+          </label>
 
-        <!-- logo -->
-        <daisyui-logo class="lg:hidden" />
+          <!-- logo -->
+          <daisyui-logo class="lg:hidden" />
+        </div>
+
+        <div class="flex-0">
+          <span class="tooltip tooltip-bottom before:text-xs before:content-[attr(data-tip)]"
+            data-tip="GitHub">
+            <div class="flex-none items-center">
+              <a aria-label="Github" target="_blank" href="https://github.com/webceyhan/nuxt-daisy-demo"
+                rel="noopener, noreferrer" class="btn btn-ghost drawer-button btn-square normal-case">
+                <ui-icon name="github" class="text-2xl" />
+              </a>
+            </div>
+          </span>
+        </div>
       </nav>
     </div>
 
