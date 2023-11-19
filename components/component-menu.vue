19caf0ce777595e3980b8d036242f6b7f89c325a
Evren Ceyhan <evren.ceyhan@gmail.com>
2023-11-19 13:58:21 +0100

    component menus extracted to component-menu

---
+++ components/component-menu.vue
@@ -0,0 +1,86 @@
+<script setup lang="ts">
+type Menu = {
+  title: string;
+  icon: string;
+  iconClass: string;
+  links: {
+    label: string;
+    href: string;
+  }[];
+};
+
+const MENU_DEFINITIONS: Record<string, Menu> = {
+  actions: {
+    title: "Actions",
+    icon: "cursor-fill",
+    iconClass: "text-fuchsia-600",
+    links: [],
+  },
+  data: {
+    title: "Data display",
+    icon: "window",
+    iconClass: "text-teal-600",
+    links: [],
+  },
+  input: {
+    title: "Data input",
+    icon: "pencil-square",
+    iconClass: "text-rose-600",
+    links: [],
+  },
+  layout: {
+    title: "Layout",
+    icon: "grid-fill",
+    iconClass: "text-blue-600",
+    links: [],
+  },
+  navigation: {
+    title: "Navigation",
+    icon: "link-45deg",
+    iconClass: "text-cyan-600",
+    links: [],
+  },
+  mockup: {
+    title: "Mockup",
+    icon: "phone",
+    iconClass: "text-lime-600",
+    links: [],
+  },
+};
+
+// populate the menu links
+useRouter()
+  .getRoutes()
+  .forEach(({ path }) => {
+    // skip the root path
+    if (path === "/") return;
+
+    // split the path into menu and page names
+    const [menuName, pageName] = path.split("/").slice(1);
+
+    MENU_DEFINITIONS[menuName].links.push({
+      label: pageName,
+      href: path,
+    });
+  });
+</script>
+
+<template>
+  <ul v-for="menu of MENU_DEFINITIONS" class="menu menu-md px-4 py-0">
+    <!-- divider -->
+    <li />
+
+    <!-- menu title -->
+    <li class="menu-title flex flex-row items-center gap-4">
+      <ui-icon :name="menu.icon" :class="['text-lg', menu.iconClass]" />
+      <span>{{ menu.title }}</span>
+    </li>
+
+    <!-- menu links -->
+    <li v-for="link of menu.links">
+      <NuxtLink :href="link.href" active-class="active">
+        <span class="capitalize">{{ link.label }}</span>
+      </NuxtLink>
+    </li>
+  </ul>
+</template>
