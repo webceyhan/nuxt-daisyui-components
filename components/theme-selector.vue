92527c8ab6f6f1600cfeb292ad19ff76113554ba
Evren Ceyhan <evren.ceyhan@gmail.com>
2023-11-26 09:51:03 +0100

    convert theme selection to composable

--- components/theme-selector.vue
+++ components/theme-selector.vue
@@ -1,48 +1,5 @@
 <script setup lang="ts">
-const themes = [
-  "light",
-  "dark",
-  // 'cupcake',
-  // 'bumblebee',
-  // 'emerald',
-  // 'corporate',
-  // 'synthwave',
-  "retro",
-  // 'cyberpunk',
-  "valentine",
-  // 'halloween',
-  // 'garden',
-  // 'forest',
-  "aqua",
-  // 'lofi',
-  // 'pastel',
-  // 'fantasy',
-  // 'wireframe',
-  // 'black',
-  "luxury",
-  // 'dracula',
-  // 'cmyk',
-  // 'autumn',
-  // 'business',
-  // 'acid',
-  // 'lemonade',
-  "night",
-  "coffee",
-  // 'winter',
-  // 'dim',
-  // 'nord',
-  // 'sunset',
-];
-
-const activeTheme = ref("dark");
-
-const onChange = (theme: string) => {
-  // append data-theme to html
-  useHead({ htmlAttrs: { "data-theme": theme } });
-
-  // set active theme
-  activeTheme.value = theme;
-};
+const { active, change, THEMES } = useTheme();
 </script>
 
 <template>
@@ -84,11 +41,9 @@ const onChange = (theme: string) => {
       <div tabindex="0" class="grid grid-cols-1 gap-3 p-3">
         <!-- theme 1 -->
         <button
-          v-for="theme of themes"
+          v-for="theme of THEMES"
           class="outline-base-content overflow-hidden rounded-lg text-left"
-          data-set-theme="light"
-          data-act-class="[&amp;_svg]:visible"
-          @click="onChange(theme)"
+          @click="change(theme)"
         >
           <span
             :data-theme="theme"
@@ -105,7 +60,7 @@ const onChange = (theme: string) => {
                   viewBox="0 0 24 24"
                   fill="currentColor"
                   class="h-3 w-3 shrink-0"
-                  :class="{ invisible: theme !== activeTheme }"
+                  :class="{ invisible: theme !== active }"
                 >
                   <path
                     d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"
