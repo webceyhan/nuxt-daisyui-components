20f01b83662eb6f97b5b4935d81895948acac679
webceyhan <evren.ceyhan@gmail.com>
2023-12-23 21:26:24 +0000

    Deploying to gh-pages from @ webceyhan/nuxt-daisyui-components@ca8e082fc85fcd8051c64edd890e883678d33e39 🚀

---
+++ _nuxt/shoe.f23366b2.js
@@ -0,0 +1,1 @@
+import"./entry.34dfefe9.js";const o=""+globalThis.__publicAssetsURL("img/shoe.jpg");export{o as _};
---
+++ _nuxt/spiderman.f5ffdaa2.js
@@ -0,0 +1,1 @@
+import"./entry.34dfefe9.js";const i=""+globalThis.__publicAssetsURL("img/spiderman.jpg");export{i as _};
