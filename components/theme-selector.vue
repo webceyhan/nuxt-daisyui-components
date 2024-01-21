e7e8c83658e84e0296931b4bfcd30623ad9259ae
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-01-21 12:41:36 +0100

    add mobile version of theme selector

--- components/theme-selector.vue
+++ components/theme-selector.vue
@@ -3,7 +3,29 @@ const { active, change, THEMES } = useTheme();
 </script>
 
 <template>
-  <ui-dropdown label="Theme" align-to-end ghost>
+  <ui-dropdown align-to-end>
+    <template #trigger>
+      <ui-button ghost>
+        <svg
+          width="20"
+          height="20"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          class="h-5 w-5 stroke-current md:hidden"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"
+          ></path>
+        </svg>
+        <span class="hidden md:inline">Theme</span>
+        <ui-icon name="chevron-down" class="hidden sm:inline-block" />
+      </ui-button>
+    </template>
+
     <li v-for="theme of THEMES">
       <a @click="change(theme)" class="block rounded-lg p-1">
         <span
