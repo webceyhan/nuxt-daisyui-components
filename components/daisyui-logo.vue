dbf75dbb20533f7900fa33d87ca740a26b4a8e3f
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-01-21 12:55:29 +0100

    updated daisyUI logo

--- components/daisyui-logo.vue
+++ components/daisyui-logo.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="flex items-center gap-2 px-4 py-2 backdrop-blur">
-    <NuxtLink href="/" class="flex-0 btn btn-ghost px-2">
+  <div class="flex items-center gap-2 md:px-4 py-2 backdrop-blur">
+    <NuxtLink href="/" class="flex-0 btn btn-ghost gap-1 px-2 md:gap-2">
       <svg
+        class="h-6 w-6 md:h-8 md:w-8"
         width="32"
         height="32"
         viewBox="0 0 415 415"
@@ -12,13 +13,10 @@
         <circle cx="207.5" cy="135" r="125" fill="white"></circle>
         <circle cx="207.5" cy="135" r="56" fill="#FF9903"></circle>
       </svg>
-      <div class="font-title inline-flex text-lg md:text-2xl">
-        <span class="lowercase">daisy</span>
-        <span class="uppercase text-[#1AD1A5]">UI</span>
-      </div>
+      <span class="font-title text-base-content text-lg md:text-2xl">daisyUI</span>
     </NuxtLink>
 
-    <i class="text-lg font-serif opacity-50">for Nuxt</i>
+    <i class="text-sm md:text-lg font-serif opacity-50">for Nuxt</i>
   </div>
 </template>
 
