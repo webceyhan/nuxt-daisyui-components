c20f9d4f62c6327564e5f62fa32c5fe3f057fe12
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-01-22 18:45:39 +0100

    move svg icons to icons.svg container

--- pages/components/drawer/index.vue
+++ pages/components/drawer/index.vue
@@ -52,19 +52,7 @@ import { DESCRIPTIONS } from "./_data";
         <div class="w-full navbar bg-base-300">
           <div class="flex-none">
             <label for="drawer-3" class="btn btn-square btn-ghost">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                class="inline-block w-6 h-6 stroke-current"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                ></path>
-              </svg>
+              <svg-icon name="menu-toggle" class="w-6 h-6" />
             </label>
           </div>
           <div class="flex-1 px-2 mx-2">Navbar Title</div>
